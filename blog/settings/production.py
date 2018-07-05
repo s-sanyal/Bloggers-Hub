0984320cbc8e8c9111dce84ba461ed4ad4841256
SAYAN SANYAL <bluesayanhouse@gmail.com>
2018-07-05 15:10:14 +0530

    Updated settings module

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
