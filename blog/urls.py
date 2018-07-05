7facd872935ffadbbcd1c94f914f27bc5046ce74
SAYAN SANYAL <bluesayanhouse@gmail.com>
2018-07-05 18:58:22 +0530

    Updated certain stylesheets

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,3 @@
-
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
@@ -8,6 +7,6 @@ urlpatterns = [
     url(r'^posts/', include('posts.urls')),
 ]
 
-# if settings.DEBUG:
-# urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-# urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
