5a85085140e8113e2911d724cbb486470ae51d7f
SAYAN SANYAL <bluesayanhouse@gmail.com>
2018-07-05 14:15:05 +0530

    urls updated

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^posts/', include('posts.urls')),
 ]
 #if settings.DEBUG:
-urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+#urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+#urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 app_name='posts'
 urlpatterns = [
     # /posts/
@@ -39,3 +41,5 @@ urlpatterns = [
     #/posts/searched/<searched_item>/
     url(r'^searched/$',views.search_result,name="search"),
     ]
+#urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+#urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
