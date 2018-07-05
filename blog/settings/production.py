d9a704ece1081f371124ac92f483bbad12ea25b0
SAYAN SANYAL <bluesayanhouse@gmail.com>
2018-07-05 18:00:43 +0530

    Updated static path

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 #MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
