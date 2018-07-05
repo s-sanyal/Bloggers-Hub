f64c4ebe88bfdf4e3a54c886e24d9be189a6e860
SAYAN SANYAL <bluesayanhouse@gmail.com>
2018-07-05 15:31:32 +0530

    Updated settings module

--- blog/urls.py
+++ blog/urls.py
@@ -7,11 +7,8 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include('posts.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+
 #if settings.DEBUG:
 #urlpatterns+=(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
-#urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
